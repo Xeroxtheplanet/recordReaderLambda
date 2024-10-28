27a517d6eff07331295295071e41ca6c78c036c3
Xeroxtheplanet <109957741+Xeroxtheplanet@users.noreply.github.com>
2024-10-28 11:25:21 -0700

    Ready

--- src/milestone3/Pass.java
+++ src/milestone3/Pass.java
@@ -15,35 +15,22 @@ import java.util.regex.*;
 import java.lang.Object.*;
 
 
-public class Pass {
+public class Pass { //By Lachlan Miller
 	String answer = "";
 	int recordsValidated = 0;
-	//List<CRecord> recordList = new ArrayList<CRecord>(); //set pointer to recordArr
-	//Map<String,String> obj = new HashMap<String,String>();
 	
-
-	/*Stream<CRecord> recordStream = ArrayList.stream(recordList).map(((CRecord r) -> r)
-								.filter(((CRecord f)->f)
-								.isValid==true));*/
-	//recommended snippets of code
-	
-	class Record { //inner class Record with fields transcribed from hashmap.
+	class Record { //inner class Record with fields transcribed from map.
 		
 		String recordStr = "-".repeat(132)+"\n{";
-		int fieldsValidated = 0; //if the value does not 
+		int fieldsValidated = 0;
 		
-		//HashMap<String, String> recordkvToValidate; //pointer to hashmap
-		
-		/*construct and validate the record here by going through hashmap
-		pairs and determining if valid. if the number of fields validated is 7
+		/*construct and validate the record here by going through map
+		pairs and determining if valid. if the number of fields validated is 7 or more, go it is fine
 		*/
 
 		public Record(Map<String,String> recordKVToValidate) throws NumberFormatException {  
 			recordKVToValidate.forEach((k, v) -> { //accepts key and value to do whatever, less verbose than constructing iterators
-				boolean KVCheck = false;
-				Integer vVal = 0;
-				String hairAccept = "123456789abcdef#"; //acceptable hex characters for hair
-				String licenseAccept = "0123456789";
+				Integer vVal = 0; //used to 
 				if(v.contains("cm")){
 					vVal = Integer.parseInt(v.replace("cm", "")); //strip of cm if it contains it
 					vVal = vVal.intValue();//return int primitive value
@@ -70,13 +57,11 @@ public class Pass {
 												  	v.equals("hazel") ||
 												  	v.equals("other")
 												  	)) ||
-						(k.equals("usmca") && (v.matches("^[0-9_]+$") &&
+						(k.equals("usmca") && (v.matches("^[0-9_]+$") && //only digits
 												   	v.length()==9)
 						)
 					)
 					{
-						System.out.println(k+ " was valid key found value " + v); //TEST: delete when done
-						KVCheck=true; //confirmed that key is valid for validation of pair
 						fieldsValidated+=1;
 					}
 				} catch (NumberFormatException e){
@@ -99,44 +84,17 @@ public class Pass {
 				recordsValidated+=1;
 				return recordStr;
 			}
-			//return recordStr+"\n"; //if null, it is not a valid record so add nothing
-			return "";
+			return ""; //return nothing if invalid record
 		}
 	}
-	
-	
-	
-
-	
-	/* print each record separated by a dashed line
-
-	.peek(str -> {
-		System.out.println("-".repeat(132));
-		System.out.println(str);  
-	})*/
 	
 
 	public static void main(String[] args) {
 		
-		// TODO Auto-generated method stub
-		/*My solution:
-		1. add all the lines in the text file to a list (I may need to make a new class for this)
-
-		//set up a stream that filters the results of the list based on 
-		whether they fill certain prerequisites listed below
-		// print out the results at the end of this all.
-		
-		This project will require:
-		stream api,
-		regex api
-		io api
-		buffered reading api
-		*/
 		try{
-			Pass c = new Pass(); // have to make a class to have an inner class
+			Pass c = new Pass(); // have to make a class to have an inner class to fit submission
 			
 			Stream<String> lines = Files.lines(Paths.get("data/paths.txt"));
-			//lines.forEach(System.out::println);
 			Stream<String> myStreamOfLines = Arrays.stream(lines.collect(Collectors.joining("\n")).split("\n{2}")); 
 			// Parse input into records on basis regex basis \n{2} (two consecutive \n) (note: joining on \n just joins them together so they are treated as one element in the stream)
 
@@ -145,14 +103,10 @@ public class Pass {
 			myStreamOfLines.forEach(str -> {
 				Map<String,String> obj = Arrays.stream(str.split(" |\n")) //for each string, split on regex twice first on the spaceor a new line then on the kvpair
 											.map(kvPair -> kvPair.split(":"))
-											//.filter(Arr -> Arr.length==2) probably not needed
+											.filter(Arr -> Arr.length==2) //in case duplicates for whatever reason eg. : is in a key or value for some reason
 											.collect(Collectors.toMap(arr -> arr[0], arr -> arr[1])); //buffer for validation
-    			//String[] parts = str.split(":"); //split each section on basis of delimiter : not needed anymore
-    			//obj.put(parts[0], parts[1]);
 				Record recordToCheck = c.new Record(obj);
 				c.answer+=recordToCheck.getRecordStr();
-				//c.answer+="TEST: Fields validated for record is " + recordToCheck.fieldsValidated+"\n";
-				//System.out.println(obj.get("hair"));
 				
 				obj.clear(); //clear for next record
 
@@ -160,9 +114,6 @@ public class Pass {
 			
 
 			System.out.println(c.answer + "=".repeat(132) + "\nValid records: "+ c.recordsValidated); //end it with this
-			
-
-
 
 		} catch (Exception e){
 			e.printStackTrace();
