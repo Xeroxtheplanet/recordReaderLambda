58f9ea00ef47d13721b76f698591536f5b6e6104
Xeroxtheplanet <109957741+Xeroxtheplanet@users.noreply.github.com>
2024-10-28 09:13:52 -0700

    Nearly done, regex, record building, functional streams

--- src/milestone3/Pass.java
+++ src/milestone3/Pass.java
@@ -11,11 +11,14 @@ import java.util.stream.Collectors.*;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.regex.*;
+import java.lang.Object.*;
 
 
 public class Pass {
+	String answer = "-".repeat(132)+"\n";
 	//List<CRecord> recordList = new ArrayList<CRecord>(); //set pointer to recordArr
-	Map<String,String> obj = new HashMap<String,String>();
+	//Map<String,String> obj = new HashMap<String,String>();
 	
 
 	/*Stream<CRecord> recordStream = ArrayList.stream(recordList).map(((CRecord r) -> r)
@@ -23,61 +26,79 @@ public class Pass {
 								.isValid==true));*/
 	//recommended snippets of code
 	
-	class Record {
+	class Record { //inner class Record with fields transcribed from hashmap.
+		
 		String recordStr = "{";
 		int fieldsValidated = 0; //if the value does not 
-		HashMap<String, String> recordkvToValidate;
+		//HashMap<String, String> recordkvToValidate; //pointer to hashmap
 		
 		/*construct and validate the record here by going through hashmap
 		pairs and determining if valid. if the number of fields validated is 7
 		*/
 
-		public Record(HashMap<String,String> recordKVToValidate){  
+		public Record(Map<String,String> recordKVToValidate) throws NumberFormatException {  
 			recordKVToValidate.forEach((k, v) -> { //accepts key and value to do whatever, less verbose than constructing iterators
 				boolean KVCheck = false;
 				Integer vVal = 0;
 				String hairAccept = "123456789abcdef#"; //acceptable hex characters for hair
-				String licenseAccept = "123456789";
+				String licenseAccept = "0123456789";
 				if(v.contains("cm")){
 					vVal = Integer.parseInt(v.replace("cm", "")); //strip of cm if it contains it
+					vVal = vVal.intValue();//return int primitive value
 				}
 				if(v.contains("in")){
-					vVal = Integer.parseInt(v.replace("in", "")); //strip of int if it contains it
+					vVal = Integer.parseInt(v.replace("in", "")); //strip of in if it contains it
+					vVal = vVal.intValue(); //return int primitive value
 				}
-				if (k.equals("born") && ((2024 - vVal)>21) || //over age 21
-					k.equals("issued") && vVal<=2024 && (2024-Integer.parseInt(v))<=10 || //before or on 2024 and was issued less than 10 years ago
-					k.equals("expires") && vVal>2024 || //ensure it has not expired
-					k.equals("height") && ((vVal>150 && vVal<193) || (vVal>59 && vVal<76)) || //between 150cm and 193cm inclusive or height between 59 and 76 inches
-					k.equals("hair") && (
-												v.startsWith("#") && 
-												hairAccept.indexOf(v)!=-1 && //contains the acceptable characters, would return -1 if no
-												v.length()==6
-												) ||
-					k.equals("eyes") && (v.equals("amber") ||
-												  v.equals("blue") ||
-												  v.equals("brown") ||
-												  v.equals("gray") ||
-												  v.equals("green") ||
-												  v.equals("hazel") ||
-												  v.equals("other")
-												  ) ||
-					k.equals("usmca") && (licenseAccept.indexOf(v)!=-1 &&
-												   v.length()==9)
-				)
-				{
-					System.out.println("valid key found"); //TEST: delete when done
-					KVCheck=true; //confirmed that key is valid for validation of pair
-					fieldsValidated+=1;
+				try{
+					if ((k.equals("born") && ((2024 - vVal)>21)) || //over age 21
+						(k.equals("issued") && Integer.parseInt(v)<=2024 && (2024-Integer.parseInt(v))<=10) || //before or on 2024 and was issued less than 10 years ago
+						(k.equals("expires") && Integer.parseInt(v)>2024) || //ensure it has not expired
+						(k.equals("height") && ((vVal>150 && vVal<193) || (vVal>59 && vVal<76))) || //between 150cm and 193cm inclusive or height between 59 and 76 inches
+						(k.equals("hair") && (
+													v.startsWith("#") && 
+													v.matches("^[#a-f0-9_]+$") && //contains the acceptable characters
+													v.length()==7
+													)) ||
+						(k.equals("eyes") && (v.equals("amber") ||
+												  	v.equals("blue") ||
+												  	v.equals("brown") ||
+												  	v.equals("gray") ||
+												  	v.equals("green") ||
+												  	v.equals("hazel") ||
+												  	v.equals("other")
+												  	)) ||
+						(k.equals("usmca") && (licenseAccept.indexOf(v)!=-1 &&
+												   	v.length()==9)
+						)
+					)
+					{
+						System.out.println(k+ " was valid key found value " + v); //TEST: delete when done
+						KVCheck=true; //confirmed that key is valid for validation of pair
+						fieldsValidated+=1;
+					}
+				} catch (NumberFormatException e){
+					System.out.println("NumberformatException detected");
 				}
-				recordStr += (k+"="); //add the key to the recordStr, regardless of what it is
-				recordStr += (v+","); //complete the kv pair
-				
+				finally{
+					recordStr += (k+"="); //add the key to the recordStr, regardless of what it is
+					recordStr += (v+", "); //complete the kv pair
+					
+				}
 			});
 
-			recordStr += "}"; //complete the record once each pair has been dealt with
+			recordStr += "\b\b}"; //complete the record once each pair has been dealt with and 2 backslash to reduce comma and space at end
+			
 
-			}
 		}
+
+		String getRecordStr(){ //getter for recordStr
+			if(fieldsValidated>=7){ //if the fields validated is seven or more than return the string
+				return recordStr;
+			}
+			return recordStr+"\n"; //if null, it is not a valid record so add nothing
+		}
+	}
 	
 	
 	
@@ -92,6 +113,7 @@ public class Pass {
 	
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		/*My solution:
 		1. add all the lines in the text file to a list (I may need to make a new class for this)
@@ -107,26 +129,42 @@ public class Pass {
 		buffered reading api
 		*/
 		try{
+			Pass c = new Pass(); // have to make a class to have an inner class
+			
 			Stream<String> lines = Files.lines(Paths.get("data/paths.txt"));
 			//lines.forEach(System.out::println);
-			Stream<String> myStreamOfLines = Arrays.stream(lines.collect(Collectors.joining("\n")).split("\n{2}")); // Parse input into records on basis \n
-			//System.out.println(myStreamOfStrings.findFirst().toString().charAt(0));
-			myStreamOfLines.forEach(System.out::println);
+			Stream<String> myStreamOfLines = Arrays.stream(lines.collect(Collectors.joining("\n")).split("\n{2}")); 
+			// Parse input into records on basis regex basis \n{2} (two consecutive \n) (note: joining on \n just joins them together so they are treated as one element in the stream)
 
-			Map<String,String> obj = new HashMap<String,String>();
+			
+			
 			myStreamOfLines.forEach(str -> {
-    			String[] parts = str.split(":");
-    			obj.put(parts[0], parts[1]);
+				Map<String,String> obj = Arrays.stream(str.split(" |\n")) //for each string, split on regex twice first on the spaceor a new line then on the kvpair
+											.map(kvPair -> kvPair.split(":"))
+											.filter(Arr -> Arr.length==2)
+											.collect(Collectors.toMap(arr -> arr[0], arr -> arr[1])); //buffer for validation
+    			//String[] parts = str.split(":"); //split each section on basis of delimiter : not needed anymore
+    			//obj.put(parts[0], parts[1]);
+				Record recordToCheck = c.new Record(obj);
+				c.answer+=recordToCheck.getRecordStr();
+				c.answer+="TEST: Fields validated for record is " + recordToCheck.fieldsValidated+"\n";
+				c.answer+="-".repeat(132)+"\n";
+				//System.out.println(obj.get("hair"));
+				//TEST:
+				
+				obj.clear(); //clear for next record
+
   			});
+			
+
+			System.out.println(c.answer);
+			
+
+
 
 		} catch (Exception e){
 			e.printStackTrace();
-		}//produces a stream of lines
-		
-		
-		// Parse colon separated key/value pairs into a HashMap
-		
-		//The hashmap obj has the string elements delimited by : 
+		}
 		
 		
 		
